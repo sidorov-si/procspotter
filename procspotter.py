fde061d3db164c0fbc58b88f897467b086cae99e
Myco <svet.sidorov@gmail.com>
2016-07-21 14:20:04 +0300

    Replaced command with PID. Corrected stat formatting.

--- procspotter.py
+++ procspotter.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 """
 Monitor a process resource usage with pidstat utility.
-Process here is a command set with -c argument.
-is simple wrapper helps to avoid some manual work:
-first, to start a command, then - to find its PID (process
-id), and finally start pidstat utility to watch it.
+Process here is defined with a PID set with -w argument.
 
 sysstat package must be installed before the first use of
 procspotter (it contains pidstat).
 
 For all procspotter arguments you can find short explanations
-below. --args argument though is worth explaining a little 
+below. '--args' argument though is worth explaining a little 
 deeper. You want to use --args when some specific 
 statistics should be collected. The value of this argument
 should be set in double quotes.
@@ -18,16 +15,16 @@ should be set in double quotes.
 '--args' argument enables you to pass any arguments to pidstat.
 By default, pidstat is launched with the following arguments:
 
-pidstat -p PID -d -r -u 2 > <pidstat_log_name>
+pidstat -p PID -d -r -u 2 > pidstat_log_name
               |__________|
 
-where -p sets the process ID that is determined 
-         automatically by procspotter,
+where -p sets the process ID that is defined 
+         in -w ('watch') argument of procspotter,
       -d is for disk I/O statistics,
       -r is for memory utilization statistics,
       -u is for CPU utilization statistics,
-      2 is an interval (in seconds) between two measures.
-      <pidstat_log_name> is set with -l argument of procspotter.
+      2 is an interval (in seconds) between two measurements.
+      pidstat_log_name is set with -l argument of procspotter.
 
 'count' argument for pidstat is not set here, so pidstat 
 will collect statictics until the process finishes.
@@ -41,46 +38,42 @@ E. g., you may want to set 'interval' and 'count' arguments
 for pidstat and to collect only CPU statistics.
 You can do it with '--args' as follows:
 
-procspotter.py -c <command> -l <pidstat_lot_name> --args "-u 2 10"
+procspotter.py -w PID -l pidstat_log_name --args "-u 2 10"
 
 where 2 is a value of 'interval' argument and 10 is a value
 of 'count' argument.
 
-When -p argument is given, a pidstat log file is parsed, and 
-some overall statistics are calculated and output - maximum and
-average value for each characteristic. If a characteristic is 
-not numerical (e. g., 'Command'), corresponding statistics are 
-set to zero.
+When -p ('parse') argument is given to procspotter, a pidstat log file 
+is parsed and maximum and average values are calculated for all 
+statistics measured by pidstat. If some statistic is not numerical 
+(e. g., 'Command'), corresponding statistics are set to zero.
 
 Please note that:
-1) neither the <command> nor the value of '--args' argument 
-   is checked for correctness and safety;
-2) the <command> must be executed at least for several seconds for 
-   pidstat to collect some statistics; for 1 - 3 seconds at the
-   very begining statistics is not collected;
-3) <command> mustn't contain output redirectiron ('>') or 
-   pipes ('|');
-4) if -u argument for pidstat is set manually via --args argument,
-   then its value should be less than a day;
-5) <command> will output to stdout and/or stderr just as if
-   it were launched without procspotter;
+1) neither a PID nor a value of '--args' argument is checked for 
+   correctness and safety;
+2) process with a given PID must be executed at least for several 
+   seconds for pidstat to collect some statistics; for 1 - 3 seconds 
+   at the very begining statistics are not collected;
+3) if -u argument for pidstat is set manually via '--args' argument,
+   then its value should be less than one day;
+5) process with a given PID will output to stdout and/or stderr 
+   just as if it were launched without procspotter;
 6) with -p argument statistics for all characteristics are calculated 
-   and output, so for fields like 'Command' or 'CPU' it shouldn't be
-   considered;
+   and output, so for fields like 'Command' or 'CPU' (CPU ID) it 
+   shouldn't be considered;
 7) time utility shouldn't be used within the command: procspotter's 
    log parser will print total execution time anyway.
 
 Usage:
-  procspotter.py -c <command> -l <pidstat_log_name> [--verbose --args <arguments_for_pidstat>]
+  procspotter.py -w <PID> -l <pidstat_log_name> [--args <arguments_for_pidstat>]
   procspotter.py -p <pidstat_log_name> 
 
 Options:
   -h --help                       Show this screen.
   --version                       Show version.
-  -c <command>                    Command to be watched with pidstat.
+  -w <PID>                        PID of the process to be watched with pidstat.
   -l <pidstat_log_name>           Name of a log file for pidstat output.
   -p <pidstat_log_name>           Parse log file and print statistics.
-  --verbose                       Print info about the launched <command>.
   --args <arguments_for_pidstat>  Arguments for pidstat. Default: ''.
 """
 
@@ -113,18 +106,17 @@ from os.path import isfile
 
 
 def print_stats(total_time, headers, values, log_name):
-    print
     print "Statistics from", log_name + ':'
     print 
     print "Total time:", total_time
     print
     stdout.flush()
     for header_index, header in enumerate(headers):
-        current_max_list = [str(dictionary["maximum"]) for dictionary in values[header_index]]
-        current_avg_list = [str(dictionary["avg"]) for dictionary in values[header_index]]
+        current_max_list = [dictionary["maximum"] for dictionary in values[header_index]]
+        current_avg_list = [dictionary["avg"] for dictionary in values[header_index]]
         header_line = ' ' * 8 + ''.join(['{0: <14}'.format(head) for head in header])
-        max_line = 'Max:    ' + ''.join(['{0: <14}'.format(value) for value in current_max_list])
-        avg_line = 'Avg:    ' + ''.join(['{0: <14}'.format(value) for value in current_avg_list])
+        max_line = 'Max:    ' + ''.join(['{0: <14.3f}'.format(value) for value in current_max_list])
+        avg_line = 'Avg:    ' + ''.join(['{0: <14.3f}'.format(value) for value in current_avg_list])
         print header_line
         print max_line
         print avg_line
@@ -213,38 +205,30 @@ def parse_pidstat(log_name):
         print_stats(total_time, headers, values, log_name)
 
 
-def set_pidstat(command, log_name, verbose, args):
+def set_pidstat(pid, log_name, args):
     with open(log_name, 'w') as log:
-        command_list = command.split()
-        process = subprocess.Popen(command_list, close_fds = True)
-        pid = str(process.pid)
-        if verbose:
-            print 'The following command is launched:'
-            print command
-            print 'PID:', pid
-            print 'All statistics are being logged in', log_name
-            stdout.flush()
+        print 'The following process is watched (PID):', pid
+        print 'All statistics are being logged in', log_name
+        stdout.flush()
         pidstat_args = args if args != None else '-d -r -u 2'
         pidstat_args_list = pidstat_args.split()
         pidstat_list = ['pidstat', '-p', pid] + pidstat_args_list
         pidstat_process = subprocess.Popen(pidstat_list, stdout = log)
         pidstat_pid = str(pidstat_process.pid)
-        if verbose:
-            print 'Statistics are being collected with the following pidstat process:'
-            print ' '.join(pidstat_list), '>', log_name
-            print 'PID of the pidstat process:', pidstat_pid
-            stdout.flush()
+        print 'Statistics are being collected with the following pidstat process:'
+        print ' '.join(pidstat_list), '>', log_name
+        print 'PID of the pidstat process:', pidstat_pid
+        stdout.flush()
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='procspotter 0.2')
+    arguments = docopt(__doc__, version='procspotter 0.3')
     case = ""
-    if arguments["-c"] != None:
+    if arguments["-w"] != None:
         case = "watch"
-        command = arguments["-c"]
+        pid = arguments["-w"]
         log_name = arguments["-l"]
         args = arguments["--args"]
-        verbose = arguments["--verbose"] # True or False
     elif arguments["-p"] != None:
         case = "parse"
         log_name = arguments["-p"]
@@ -258,7 +242,7 @@ if __name__ == '__main__':
             sys.exit(1)
     
     if case == "watch":
-        set_pidstat(command, log_name, verbose, args)
+        set_pidstat(pid, log_name, args)
     elif case == "parse":
         parse_pidstat(log_name)
 
